4d8156203786a6e9dc33ea15644285b892b7e46a
sufyanahmed7 <su.sufyanahmed@gmail.com>
2024-04-28 23:35:23 +0500

    Rendering Items lists

---
+++ travel-list/src/Item.jsx
@@ -0,0 +1,9 @@
+function Item({ item }) {
+  return (
+    <div>
+      <li>{item.description}</li>
+    </div>
+  );
+}
+
+export default Item;
--- travel-list/src/PackingList.jsx
+++ travel-list/src/PackingList.jsx
@@ -1,4 +1,15 @@
+import Item from "./Item";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
 }
 export default PackingList;
