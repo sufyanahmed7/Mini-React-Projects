fa8c571af8199a783cdf843900f8070a984d8c0d
sufyanahmed7 <su.sufyanahmed@gmail.com>
2024-05-05 21:16:50 +0500

    Trying multiple objects

--- travel-list/src/PackingList.jsx
+++ travel-list/src/PackingList.jsx
@@ -3,6 +3,7 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 2, description: "Spoon", quantity: 12, packed: true },
+  { id: 2, description: "Spoon", quantity: 12, packed: true },
 ];
 function PackingList() {
   return (
